58f4e575d6705e41cd2a4a637607f75132e0193e
ThomasRny <33687666+ThomasRny@users.noreply.github.com>
2019-12-05 11:57:19 +0100

    initialized page d'attente

---
+++ countdown-en.js
@@ -0,0 +1,23 @@
+// COUNTDOWN
+
+var countDownDate = new Date("Jun 17, 2020 09:00:00").getTime();
+
+var x = setInterval(function() {
+var now = new Date().getTime();
+var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+  "<p class='nombrecountdown'><span class='chiffrecountdown'>" + days + "</span> <span class='textcountdown'> Days </span> </p> <p class='deuxpoint'>:</p>" + 
+  "<p class='nombrecountdown'><span class='chiffrecountdown'>" + hours + "</span> <span class='textcountdown'> Hours </span> <p class='deuxpoint'>:</p>" + 
+  "<p class='nombrecountdown'><span class='chiffrecountdown'>" + minutes + "</span> <span class='textcountdown'> Minutes </span> <p class='deuxpoint'>:</p>" +
+  "<p class='nombrecountdown'><span class='chiffrecountdown'>" + seconds + "</span> <span class='textcountdown'> Seconds </span>" ;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
